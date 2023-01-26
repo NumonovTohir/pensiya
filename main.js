1497c1a0657fc50d336b7213caccae598817ea8f
NumonovTohir <numonovtohir0109@gmail.com>
2023-01-26 14:46:41 +0300

    pensiya yoshi

---
+++ main.js
@@ -0,0 +1,10 @@
+let pensioner = prompt("Yoshingizni kiriting");
+let pensiyaYoshi = 60;
+if (pensioner > 55) {
+  alert("Siz pensionersiz!");
+  console.log("Siz pensionersiz!");
+} else {
+  alert("Hali pensiya yoshida emassiz!");
+  console.log("Hali pensiya yoshida emassiz!");
+  console.log(pensiyaYoshi - pensioner);
+}
